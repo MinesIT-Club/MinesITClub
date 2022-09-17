28d2653060b93a5edfb353c910c6b279cc920f11
Maddouchhamza <maddouch.hamza@gmail.com>
2022-09-17 19:16:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var nav = document.getElementById("link1");
+var chec = document.getElementById("navi-toggle");
+nav.addEventListener("click", CloseNav);
+
+function CloseNav() {
+
+    chec.checked = false;
+
+};
